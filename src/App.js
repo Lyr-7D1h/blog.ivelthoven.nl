a8b6c29a8f5f0af6213315dc792754091ca2778d
Ivo Velthoven <ivelthoven@protonmail.com>
2019-10-26 15:24:11 +0200

    fixed routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from "react";
 import "./App.css";
 import "antd/dist/antd.css";
-import { BrowserRouter, Route } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
 import ProjectsPage from "./pages/ProjectsPage";
 import AdminPage from "./pages/AdminPage";
 
-const supportsHistory = "pushState" in window.history;
+// const supportsHistory = "pushState" in window.history;
 
 function App() {
   return (
-    <BrowserRouter forceRefresh={!supportsHistory}>
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/projects" component={ProjectsPage} />
-      <Route exact path="/admin" component={AdminPage} />
-    </BrowserRouter>
+    <HashRouter>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/projects" component={ProjectsPage} />
+        <Route exact path="/admin" component={AdminPage} />
+      </Switch>
+    </HashRouter>
   );
 }
 
