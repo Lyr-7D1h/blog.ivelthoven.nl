6d063e94984ffe38353edfaaa84e026c69b4d8da
Ivo Velthoven <ivelthoven@protonmail.com>
2019-10-26 13:45:15 +0200

    Updates

---
+++ precache-manifest.78af05359601bf90c23186986b801ca9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39d658d721c5e5102e520f3cf3ee06ad",
+    "url": "./index.html"
+  },
+  {
+    "revision": "757cc1a7c522b7d7523c",
+    "url": "./static/css/2.aa7fcd32.chunk.css"
+  },
+  {
+    "revision": "955bbea6a256f15641a4",
+    "url": "./static/css/main.e4e7d7bf.chunk.css"
+  },
+  {
+    "revision": "757cc1a7c522b7d7523c",
+    "url": "./static/js/2.d1d7ae18.chunk.js"
+  },
+  {
+    "revision": "955bbea6a256f15641a4",
+    "url": "./static/js/main.14d8e616.chunk.js"
+  },
+  {
+    "revision": "854c02ac798447d8460d",
+    "url": "./static/js/runtime-main.60135f80.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blog.ivelthoven.nl/precache-manifest.a9b9618d95074dde86c8c9c81685efc1.js"
+  "./precache-manifest.78af05359601bf90c23186986b801ca9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/blog.ivelthoven.nl/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
